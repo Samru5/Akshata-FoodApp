904426cc5241ee8f930e63a62850dc9b54b0fa85
Shetye <samruddhi-dinesh.shetye@capgemini.com>
2019-05-06 19:28:10 +0530

    add

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.route';
+// import { OrderModule } from 'src/order/order.module';
+import { WelcomeComponent } from './welcome.component';
+import { CustomerModule } from './customer/customer.module';
+// import { DishComponent } from 'src/order/dish.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    // DishComponent
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    // OrderModule,
+    CustomerModule,
+    RouterModule.forRoot(appRoutes)
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.route.ts
@@ -0,0 +1,9 @@
+import { AppComponent } from './app.component';
+import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome.component';
+
+export const appRoutes:Routes=[
+    {path:"home" ,component:WelcomeComponent},
+      {path:"" ,component:WelcomeComponent}
+
+]
